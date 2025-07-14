da0234cfa5ed123114addd7259c23e9728030526
Samuel Nimblad <samuel.nimblad@gmail.com>
2025-07-14 15:33:25 +0200

    surf-0002: booking: Setup urls for book and cancel views

---
+++ surf_shed/booking/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from surf_shed.booking.views import cancel_booking
+from surf_shed.booking.views import new_booking
+
+app_name = "booking"
+
+urlpatterns = [
+    path("book/", view=new_booking, name="new_booking"),
+    path("cancel/", view=cancel_booking, name="cancel_booking"),
+]

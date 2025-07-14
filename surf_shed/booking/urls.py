66114471bffec79fef774d5695421c920c02a4d9
Samuel Nimblad <samuel.nimblad@gmail.com>
2025-07-14 17:41:33 +0200

    surf-0002: booking: Add views

--- surf_shed/booking/urls.py
+++ surf_shed/booking/urls.py
@@ -1,15 +1,28 @@
 from django.urls import path
 
-from surf_shed.booking.views import cancel_booking
-from surf_shed.booking.views import new_booking
+from surf_shed.booking import views
 
 app_name = "booking"
 
 urlpatterns = [
-    path("book/", view=new_booking, name="new_booking"),
-    path("cancel/", view=cancel_booking, name="cancel_booking"),
+    path("book/", view=views.new_booking, name="new_booking"),
+    path("cancel/", view=views.cancel_booking, name="cancel_booking"),
 ]
 
-htmx_patterns = []
+htmx_patterns = [
+    path(
+        "book/update_time_and_date_select/",
+        views.update_time_and_date_select,
+        name="update-time-and-date-select-view",
+    ),
+    path("book/update_check_out/", views.update_check_out, name="update-checkout-view"),
+    path("book/do_booking/", views.do_booking, name="do-booking"),
+    path("cancel/cancel_booking/", views.cancel_booking, name="cancel-booking"),
+    path(
+        "cancel/do_cancel_booking/",
+        views.do_cancel_booking,
+        name="do-cancel-booking",
+    ),
+]
 
 urlpatterns += htmx_patterns
